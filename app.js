bf777e278cdab7d914a4d9d9a3f6d3f448f35e05
trzyha <efyx83@o2.pl>
2020-04-16 13:56:35 +0200

    added check is user present functionality and register

--- app.js
+++ app.js
@@ -12,11 +12,23 @@ function submitUsr(e){
         //check if there is a record in localstorage
         if (localStorage.getItem(usrName.value) !== null){
             console.log("already in db")
+            console.log(localStorage.getItem(usrName.value));
+            //checking password is correct
+            if (usrPass.value == localStorage.getItem(usrName.value)){
+                // console.log("logged in");
+                UsrAlert("Logged in!");
+            }
+            else {
+                // console.log("wrong password")
+                UsrAlert("Wrong password");
+            }
+            
             clearForm();
         }
         //if no record in storage
         else {
-            console.log("no user in database please register")
+            // console.log("no user in database please register")
+            UsrAlert("no user in database please register");
         }
     } else {
     }
@@ -38,3 +50,14 @@ function clearForm(){
     usrName.value = '';
     usrPass.value = '';
 }
+
+function UsrAlert(getAlert) {
+    const usrAlert = document.createElement('div');
+    usrAlert.classList.add('user-alert');
+    usrAlert.innerHTML = getAlert;
+    document.body.appendChild(usrAlert);
+    setTimeout(()=>{
+        usrAlert.remove();
+    }, 2000)
+}
+
